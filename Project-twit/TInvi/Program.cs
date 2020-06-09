1d27de14c4859b5ebd12cfd986630330276ae9d3
Alex <apaige161@gmail.com>
2020-06-08 22:07:17 -0400

    can now tweet at a given later date

--- Project-twit/TInvi/Program.cs
+++ Project-twit/TInvi/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Timers;
 using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
@@ -12,14 +15,15 @@ namespace TInvi
     class Program
     {
         //schedule tasks
-
-        //search pictures by keyword
+            //calculate a new time -done
+            //post a new tweet on that new time
 
 
         //@autoBot04768645 twitter account
 
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Bot started");
 
             // Set up your credentials (https://apps.twitter.com)
@@ -39,13 +43,11 @@ namespace TInvi
                 Console.WriteLine("Could Not Login, Check Credentials");
                 Console.ResetColor();
             }
-
-
             Console.WriteLine("\n");
 
 
             //program options
-            string[] UserOptions = { "Text Only Tweet", "Picture and text", "Schedule Tweet for later", "Schedule picture for later" };
+            string[] UserOptions = { "Text Only Tweet", "Picture and Text Tweet", "Schedule Tweet for later"};
             int index = 1;
             Console.WriteLine("Choose an option by typing the number");
             for (int i = 0; i < UserOptions.Length; i++)
@@ -53,11 +55,13 @@ namespace TInvi
                 Console.WriteLine(index + "). " + UserOptions[i]);
                 index++;
             }
+            Console.WriteLine("\n");
 
             //get user input
             string userInput = Console.ReadLine();
+            Console.WriteLine("\n");
 
-            if(userInput == "1")
+            if (userInput == "1")
             {
                 /************************publish the Tweet "text" on your Timeline**************************/
                 Console.ForegroundColor = ConsoleColor.Blue;
@@ -65,25 +69,27 @@ namespace TInvi
                 Console.ResetColor();
                 string textToTweet = Console.ReadLine();
                 Tweet.PublishTweet(textToTweet + " " + DateTime.Now);
-            }
+            }   //tweet
+
             else if(userInput == "2")
             {
                 /**************************publish media with a caption*******************************/
 
                 /******promt user to pick a picture******/
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Select the number of the picture you want to post");
-                Console.WriteLine("\n");
+                Console.Write("INSTRUCTIONS: ");
                 
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Select the picture you want to post");
+                Console.WriteLine("\n");
+
+
 
                 /******working with the picture files******/
-                //set color of files
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.ForegroundColor = ConsoleColor.Black;
+                //must be in .jpg
 
-                //file name of all pictures
+                /******file name of all pictures******/
                 string pathOfPics = @"C:\Users\apaig\Documents\VSRepo\Project-twit\TInvi\twitterImg";
-                //puts each file into the array
+                //gets each file in directory
                 string[] files = Directory.GetFiles(pathOfPics);
 
                 //find file name and add to list
@@ -91,50 +97,96 @@ namespace TInvi
                 for (int iFile = 0; iFile < files.Length; iFile++)
                 {
                     //grabs each file name
-                    string fn = new FileInfo(files[iFile]).Name;
+                    string fn = new FileInfo(files[iFile]).Name;      
                     //and adds it to the list
                     fileNames.Add(fn);
                 }
 
 
+                //ask user if they want to see the list of available pictures
+                Console.Write("Would you like to see the available picture files to choose from?");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" Select: YES or NO");
+                string showAllPictureNames = Console.ReadLine();
+                Console.WriteLine("\n");
 
-                //write file names to console
-                for (int i = 0; i < fileNames.Count; i++)
+                if(showAllPictureNames == "yes" || showAllPictureNames == "" || showAllPictureNames == "y")
                 {
-                    Console.WriteLine(i + 1 + ") " + fileNames[i]);
+                    //set color of files
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    //write file names to console
+                    for (int i = 0; i < fileNames.Count; i++)
+                    {
+                        Console.WriteLine(i + 1 + ") " + fileNames[i]);
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("\n");
                 }
-                Console.ResetColor();
+
 
 
 
                 /******search picture files******/
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Would you like to search for a file by name?");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" Select: YES or NO");
+                string userSearch = Console.ReadLine().ToLower();
+                Console.WriteLine("\n");
 
-                //add user input into for loop to write all file containing search word
-                Console.WriteLine("Choose from a list of photos or search for a keyword in the filename");
-                //user input: search by keyword
-                string userSearch = Console.ReadLine();
-
-                if (userSearch != null)
+                //user input : search YES or NO
+                if (userSearch == "yes" || userSearch == "" || userSearch == "y")
                 {
-                    for (int i = 0; i < fileNames.Count; i++)
+                    //added user input into for loop to write all file containing search word
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Search for a picture file");
+                    //user input: search by keyword
+                    userSearch = Console.ReadLine();
+                    Console.WriteLine("\n");
+                    List<string> searchedFiles = new List<string>();
+                    if (userSearch != "")
                     {
-                        //searches for a keyword
-                        bool searchedFileNames = fileNames[i].Contains(userSearch);
-                        if(searchedFileNames == true)
+                        //color
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        for (int i = 0; i < fileNames.Count; i++)
                         {
-                            //add a new list or array to hold results
-                            //if no results then print a message
-                            //prints out files containing that keyword
-                            Console.WriteLine(i + 1 + ") " + fileNames[i]);
+                            //searches for a keyword
+                            bool searchedFileNames = fileNames[i].Contains(userSearch);
+                            if (searchedFileNames == true)
+                            {
+                                //list to hold results
+                                searchedFiles.Add(fileNames[i]);
+                                //prints out files containing that keyword
+                                Console.WriteLine(i + 1 + ") " + fileNames[i]);
+                            }
+                            Console.ResetColor();
                         }
-                        
+
+                        //if search doesnt result anything
+                        if (!searchedFiles.Any())
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("no results were found");
+                            Console.ResetColor();
+                            Console.WriteLine("\n");
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\n");
+                        Console.WriteLine("End of results");
+                        Console.ResetColor();
+                        Console.WriteLine("\n");
                     }
-                    Console.WriteLine("end of results");
                 }
 
+
+                /******select picture files******/
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Select the number of the picture you want to post");
-                Console.WriteLine("\n");
+                Console.WriteLine("Select the picture by entering the number to the left of it");
+                
                 Console.ResetColor();
 
 
@@ -145,15 +197,19 @@ namespace TInvi
                 
                 //full path of a file selected
                 string filePath = pathOfPics + @"\" + fileNames[realUserChoice].ToString();
-                
-                //user chooses picture caption
+
+                //user input : picture caption
+                Console.WriteLine("\n");
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("What would you like to say with your picture on twitter?");
                 Console.ResetColor();
                 //capture input
                 string textToTweet = Console.ReadLine();
+                Console.WriteLine("\n");
 
-                /**************************send tweet*******************************/
+
+
+                /**************************send picture tweet*******************************/
                 //exception unhandled if no input
                 byte[] file1 = File.ReadAllBytes(filePath);
                 var media = Upload.UploadBinary(file1);
@@ -161,34 +217,124 @@ namespace TInvi
                 {
                     Medias = new List<IMedia> { media }
                 });
-            }
+            }   // tweet media
+
             else if (userInput == "3")
             {
                 /**************************schedule a tweet*******************************/
-            }
+                DateTime currentTime = DateTime.Now;
+                Console.WriteLine("You will post a tweet at a later date");
+                Console.WriteLine("How many days do you want to wait?");
+                int userAddDays = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("How many hours do you want to wait?");
+                int userAddHours = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("How many minutes do you want to wait?");
+                int userAddMinutes = Convert.ToInt32(Console.ReadLine());
+
+                /***********************add time to the current time************************/
+
+                //get current time
+                Console.WriteLine("Current time = {0}", currentTime);
+
+                //add days, hours, minutes
+                DateTime newTime = DateTime.Now
+                    .AddDays(userAddDays)
+                    .AddHours(userAddHours)
+                    .AddMinutes(userAddMinutes);
+
+                //print time of scheduled post
+                Console.WriteLine("Your tweet will be published at " + newTime);
+
+                /*****compare times******/
+
+                //datetime compare
+                int timeCompare = DateTime.Compare(newTime, currentTime);
+                Console.WriteLine(timeCompare + " newTime, currentTime"); //1 if the added time is later than now
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("What would you like to say on twitter?");
+                Console.ResetColor();
+                string textToTweet = Console.ReadLine();
+
+                
+                /*  
+                 *  <0 − If date1 is earlier than date2
+                    0 − If date1 is the same as date2
+                    >0 − If date1 is later than date2
+                */
+
+                //waits until time == user added time
+                while (timeCompare > 0) //addTime is later than currentTime
+                {
+                    //end while loop, should end when timeCompare is changed
+                    Console.WriteLine("It is not time to post yet, the program will try again in 30 seconds...");
+                    System.Threading.Thread.Sleep(30000);   //sleep for 60 seconds
+                    timeCompare = DateTime.Compare(newTime, DateTime.Now);
+                    Console.WriteLine($"{timeCompare} ");
+
+                    if (timeCompare < 0)
+                    {
+                        break;
+                    }
+                }
+
+                //runs program
+                if (timeCompare < 0) //currentTime is later than addTime
+                {
+                        Tweet.PublishTweet(textToTweet + " " + DateTime.Now);
+                        Console.WriteLine("Tweet Should have sent at " + DateTime.Now);
+                }
+                
+
+                
+
+                /*****post tweet******/
+
+                
+                
+
+
+                while (newTime > currentTime)
+                {
+                    Console.WriteLine("It is not time to post yet, the program will try again in 60 seconds...");
+                    System.Threading.Thread.Sleep(60000);   //sleep for 60 seconds
+                    if (newTime <= currentTime)
+                    {
+                        break;
+                    }
+                }
+
+                
+
+                
+                
+
+
+            }   //tweet later
+
             else if (userInput == "4")
             {
                 /**************************schedule a picture*******************************/
-            }
+                Console.WriteLine("Schedule a tweet with a picture for later?");
+            }   //tweet media later
+
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Not an Option");
 
-            }
-
-
-            
-            
+            }   //return error
 
 
 
-
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Bot ended");
+            Console.ResetColor();
 
             Console.ReadLine();
         }
-        
 
+        
     }
 }
